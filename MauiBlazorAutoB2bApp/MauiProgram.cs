d8629399391c5443d8c25da794e2fa80482f4964
STEPHEN EDWARDS <stephen.denis.edwards@googlemail.com>
2025-05-21 11:18:43 +0100

    Configures http client

--- MauiBlazorAutoB2bApp/MauiProgram.cs
+++ MauiBlazorAutoB2bApp/MauiProgram.cs
@@ -16,10 +16,25 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
             });
 
-        // Add device-specific services used by the MauiBlazorAutoB2bApp.Shared project
-        builder.Services.AddSingleton<IFormFactor, FormFactor>();
+        //builder.Services.AddHttpClient<WeatherService>(client =>
+        //{
+	       // // client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+	       // // client.BaseAddress = new Uri("https://localhost:7250/"); // Use your API's URL
+	       // client.BaseAddress = new Uri("https://localhost:7238/"); // Use your API's URL
+        //});
 
-        builder.Services.AddMauiBlazorWebView();
+		// Add device-specific services used by the MauiBlazorAutoB2bApp.Shared project
+		builder.Services.AddSingleton<IFormFactor, FormFactor>();
+		// Register WeatherService for dependency injection
+//		builder.Services.AddScoped<WeatherService>();
+		builder.Services.AddScoped(sp =>
+			new WeatherService(new HttpClient
+			{
+				BaseAddress = new Uri("https://localhost:7038/")
+			})
+		);
+
+		builder.Services.AddMauiBlazorWebView();
 
 #if DEBUG
         builder.Services.AddBlazorWebViewDeveloperTools();
